88c99ca3eade91d3f377f627e85e6fc0848378af
Alexandru Pătrănescu <alexandru.patranescu@payu.com>
2025-12-19 17:13:51 +0200

    Add test case for when the adapter closes the stream on the workOnLocalCopy flow

--- tests/Issues/AdapterClosesStreamTest.php
+++ tests/Issues/AdapterClosesStreamTest.php
@@ -9,7 +9,10 @@
 
 namespace M2MTech\FlysystemStreamWrapper\Tests\Issues;
 
+use League\Flysystem\FilesystemOperator;
+use M2MTech\FlysystemStreamWrapper\Flysystem\FileData;
 use M2MTech\FlysystemStreamWrapper\Flysystem\StreamWrapper;
+use M2MTech\FlysystemStreamWrapper\FlysystemStreamWrapper;
 use M2MTech\FlysystemStreamWrapper\Tests\Assert;
 use M2MTech\FlysystemStreamWrapper\Tests\StreamCommand\AbstractStreamCommandTestCase;
 
@@ -30,6 +33,34 @@ class AdapterClosesStreamTest extends AbstractStreamCommandTestCase
         // adapter closes stream itself
         fclose($current->handle);
         $this->assertIsClosedResource($current->handle);
+
+        $wrapper->stream_close();
+        $this->assertIsClosedResource($current->handle);
+    }
+
+    public function testStreamCloseWithLocalCopy(): void
+    {
+        $filesystem = $this->createMock(FilesystemOperator::class);
+        FlysystemStreamWrapper::register(self::TEST_PROTOCOL, $filesystem);
+
+        $current = new FileData();
+        $current->setPath(self::TEST_PATH);
+
+        $current->handle = fopen('php://temp', 'wb');
+        $current->workOnLocalCopy = true;
+
+        $wrapper = new StreamWrapper($current);
+
+        if (!is_resource($current->handle)) {
+            $this->fail();
+        }
+
+        $filesystem->expects($this->once())
+            ->method('writeStream')
+            ->willReturnCallback(static function (string $file, $handle) {
+                // adapter closes the stream itself after writing it to file
+                fclose($handle);
+            });
 
         $wrapper->stream_close();
         $this->assertIsClosedResource($current->handle);
